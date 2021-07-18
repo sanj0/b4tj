82bd58b05db7c86005478728c3fdd84cc7557447
thomas g <malteuskoitus@gmail.com>
2021-07-18 19:08:13 +0200

    smart enemy homie

--- src/main/java/de/naclstudios/btj/menu/MainMenu.java
+++ src/main/java/de/naclstudios/btj/menu/MainMenu.java
@@ -2,8 +2,13 @@ package de.naclstudios.btj.menu;
 
 import de.edgelord.saltyengine.core.Game;
 import de.edgelord.saltyengine.core.SceneManager;
+import de.edgelord.saltyengine.core.graphics.SaltyGraphics;
+import de.edgelord.saltyengine.gameobject.DrawingRoutine;
+import de.edgelord.saltyengine.graphics.image.SaltyImage;
+import de.edgelord.saltyengine.graphics.light.StaticLightSystem;
 import de.edgelord.saltyengine.resource.InnerResource;
 import de.edgelord.saltyengine.scene.Scene;
+import de.edgelord.saltyengine.transform.Dimensions;
 import de.edgelord.saltyengine.transform.Vector2f;
 import de.edgelord.saltyengine.ui.elements.BorderedLabel;
 import de.edgelord.saltyengine.ui.elements.Button;
@@ -11,7 +16,8 @@ import de.edgelord.saltyengine.ui.elements.Label;
 import de.edgelord.saltyengine.utils.ColorUtil;
 import de.edgelord.saltyengine.utils.Positions;
 import de.edgelord.saltyengine.utils.SaltySystem;
-import de.naclstudios.btj.Level1;
+import de.naclstudios.btj.*;
+import de.naclstudios.btj.enemy.DummyEnemy;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
@@ -46,7 +52,29 @@ public class MainMenu extends Scene {
         startBtn = new Button("start", btnX, btnY0, buttonWidth, buttonHeight) {
             @Override
             public void onClick(MouseEvent e) {
-                SceneManager.setCurrentScene(new Level1());
+                SceneManager.setCurrentScene(new Scene() {
+                    @Override
+                    public void initialize() {
+                        Main.au.loop("rain");
+                        setGravity(1200);
+                        addGameObject(new Player(500, 500));
+                        addGameObject(new Obstacle(0, 900, 1000, 10, Color.BLACK, "floor"));
+                        addGameObject(new Obstacle(1200, 1200, 1000, 10, Color.BLACK, "floor1"));
+                        addGameObject(new Obstacle(2000, 1000, 1000, 10, Color.BLACK, "floor2"));
+                        addGameObject(new DummyEnemy(1200, 0, getGameObjects().get(2), (B4TJEntity) getGameObjects().get(0)));
+                        addGameObject(new DestroyableRock(1500, 1100));
+                        addDrawingRoutine(new DrawingRoutine(DrawingRoutine.DrawingPosition.BEFORE_GAMEOBJECTS) {
+                            final SaltyImage img = new InnerResource().getImageResource("assets/sam.jpeg");
+
+                            @Override
+                            public void draw(SaltyGraphics saltyGraphics) {
+                                saltyGraphics.drawImage(img, Vector2f.zero(), new Dimensions(2000, 1000));
+                            }
+                        });
+                        addGameObject(new Storm());
+                        setLightSystem(new StaticLightSystem(ColorUtil.withAlpha(ColorUtil.MIDNIGHT_BLUE, 0.35f)));
+                    }
+                });
             }
         };
         startBtn.setBackgroundColor(ColorUtil.DEEP_PINK);
